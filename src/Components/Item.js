f297b9a4369dda60922cd1da9da929d9cf90373e
Kel Strother <kelstrother@gmail.com>
2021-02-05 15:57:23 -0700

    built item and PostForm components

---
+++ src/Components/Item.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Item = () => {
+  return (
+    <div className="image-container">
+      <div className="item-image">
+        <img src="" alt="" />
+        <div className="item-cardtext">
+          <h2 className="item-cardname">50" Flatscreen TV</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
