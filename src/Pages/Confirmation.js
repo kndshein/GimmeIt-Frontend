6dbeafa8e9547f1de2b32c497c41e3f50504c916
Kaung Shein <nicholasling1412@gmail.com>
2021-02-06 05:31:29 -0500

    Cart.js, PaymentForm.js, Confirmation.js - Formatted Confirmation.js and added comments

--- src/Pages/Confirmation.js
+++ src/Pages/Confirmation.js
@@ -1,10 +1,26 @@
 import React from "react";
 
 const Confirmation = (props) => {
+  const handlePaymentConfirm = (event) => {
+    // add PUT route to add shipping address in the API
+    props.history.replace("/cart");
+  };
   return (
     <div>
       Confirmation
-      <div>{props.paymentFormData.firstName}</div>
+      <div className="buyer-container">
+        <div className="name">
+          {props.paymentFormData.firstName} {props.paymentFormData.lastName}
+        </div>
+        <div className="street-address">
+          {props.paymentFormData.addressStreet}
+          <br />
+          {props.paymentFormData.addressCity},{" "}
+          {props.paymentFormData.addressState}{" "}
+          {props.paymentFormData.addressZipcode}
+        </div>
+      </div>
+      <button onClick={handlePaymentConfirm}>Confirm</button>
     </div>
   );
 };
