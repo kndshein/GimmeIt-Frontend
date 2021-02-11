8a3954ecce0675e8465330712e00f690305f21fb
Kel Strother <kelstrother@gmail.com>
2021-02-10 18:17:09 -0700

    made logo header into a component and imported it to App.js, made desktop nav visible at all times, made image-container width formfitted

---
+++ src/Components/LogoHeader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css/Homepage.css'
+
+function LogoHeader() {
+   return (
+     <div className="home-container">
+       <div className="logo-container">
+         <h1 className="title-text">
+           Gimme<span className="dot">.</span>it
+         </h1>
+       </div>
+     </div>
+   );
+}
+
+export default LogoHeader
