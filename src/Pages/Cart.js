8134a154627167d8c1aafc14b42db0693d6ac4fd
Kaung Shein <nicholasling1412@gmail.com>
2021-02-11 21:57:52 -0500

    Added App-body in Cart.js

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -3,63 +3,66 @@ import { Route, Switch, Link } from "react-router-dom";
 
 import Payment from "./Payment";
 import Confirmation from "./Confirmation";
-import axios from"axios"
+import axios from "axios";
 
 const Cart = (props) => {
   const [paymentFormData, setPaymentFormData] = React.useState(null);
-  
+
   const handleRemoveFromCart = (item) => {
-    axios.put(props.url + "items/id/" + item._id,{
-      available: true
-    })
-    const itemId = item._id
-    props.setCartItems(props.cartItems.filter(item => item._id !== itemId))
-    console.log("cart items - ",props.cartItems)
+    axios.put(props.url + "items/id/" + item._id, {
+      available: true,
+    });
+    const itemId = item._id;
+    props.setCartItems(props.cartItems.filter((item) => item._id !== itemId));
+    console.log("cart items - ", props.cartItems);
     //make item selected unavailable
-    
-}
+  };
 
   return (
     <div>
       Cart
       {props.cartItems.map((item, index) => {
-          console.log("cart items - ", item);
-          return (
-            <div
-              className={"image-container"}
-              key={index}
-            >
-              <img className="image" src={item.img} alt="desk" />
-              <div className="image-text">
-                <h2 className="item-cardname">{item.name}</h2>
-              </div>
-              <button onClick={() => handleRemoveFromCart(item)}>Remove</button>
+        console.log("cart items - ", item);
+        return (
+          <div className={"image-container"} key={index}>
+            <img className="image" src={item.img} alt="desk" />
+            <div className="image-text">
+              <h2 className="item-cardname">{item.name}</h2>
             </div>
-          );
-        })}
-      <Link to="/cart/payment">
-        <div>Checkout</div>
-      </Link>
-      <Switch>
-        <Route
-          exact
-          path="/cart/payment"
-          render={(rp) => (
-            <Payment
-              {...rp}
-              paymentFormData={paymentFormData}
-              setPaymentFormData={setPaymentFormData}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/cart/confirmation"
-          render={(rp) => (
-            <Confirmation {...rp} paymentFormData={paymentFormData} url={props.url} cartItems={props.cartItems}/>
-          )}
-        />
-      </Switch>
+            <button onClick={() => handleRemoveFromCart(item)}>Remove</button>
+          </div>
+        );
+      })}
+      <div className="App-body">
+        <Link to="/cart/payment">
+          <div>Checkout</div>
+        </Link>
+        <Switch>
+          <Route
+            exact
+            path="/cart/payment"
+            render={(rp) => (
+              <Payment
+                {...rp}
+                paymentFormData={paymentFormData}
+                setPaymentFormData={setPaymentFormData}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/cart/confirmation"
+            render={(rp) => (
+              <Confirmation
+                {...rp}
+                paymentFormData={paymentFormData}
+                url={props.url}
+                cartItems={props.cartItems}
+              />
+            )}
+          />
+        </Switch>
+      </div>
     </div>
   );
 };
